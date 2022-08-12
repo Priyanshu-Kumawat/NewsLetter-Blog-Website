484e75e9428aa1192933e73a99f2b6a97394c80b
Priyanshu-Kumawat <87255628+Priyanshu-Kumawat@users.noreply.github.com>
2022-08-12 13:45:07 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/blogDB");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-var posts = [];
+
+
+// var posts = [];
 
 const app = express();
 
@@ -17,13 +22,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "/public")));
 
+const composeSchema = {
+
+  title: String,
+  image: String,
+  content: String
+
+};
+
+const posts = mongoose.model("posts", composeSchema);
+
 
 
 app.get("/", (req, res) => {
-  res.render("home", {
-    homeText: homeStartingContent,
-    posts:posts
-  });
+  posts.find({}, (err, postList) => {
+    if (postList.length === 0) {
+      res.render("home", { homeText: "Add Post", posts: postList });;
+    }
+    else {
+      res.render("home", { homeText: "Today", posts: postList });
+    }
+  })
 
 })
 
@@ -39,22 +58,38 @@ app.get("/compose", (req, res) => {
   res.render("compose");
 })
 
-
+app.get("/subscribe",(req,res)=>{
+  res.render("subscribe");
+})
 
 
 app.post("/compose", (req, res) => {
 
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postContent,
-    imageLink: req.body.postImageLink
-  };
 
-  posts.push(post);
+  if (posts.find({ title: {$eq: req.body.postTitles }})== true) {
+    console.log("this post is already posted.");
+    console.log(req.body.postTitles);
+    res.redirect("/compose");
+  }
+  else {
 
-  res.redirect("/");
+    console.log("not posted.");
 
-})
+    const post = new posts({
+      title: req.body.postTitle,
+      image: req.body.postImageLink,
+      content: req.body.postContent
+    });
+
+    post.save();
+    res.redirect("/");
+
+    // posts.find({}, (err, postList) => {
+    //   console.log("hlas");
+    //   res.render("home", { homeText: "haalo", posts: postList });
+    // });
+  }
+});
 
 
 
